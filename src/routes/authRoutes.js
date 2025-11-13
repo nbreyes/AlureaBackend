dab78729ba58e5a92310552885491cf6055f063c
Neil Benedict Reyes <reyesneil211@gmail.com>
2025-11-13 16:43:30 +0800

    viwrhvboiw

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/verify-otp', verifyOtp);
+router.post('/verify-cognito-email', verifyEmailCognito);
 
 export default router;
